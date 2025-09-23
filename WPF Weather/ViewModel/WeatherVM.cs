15537bc696e6185390a949757ac195586f9fc669
AndersPeh <pehjyfung@gmail.com>
2025-09-24 04:05:47 +1000

    Setup Two Way Data Binding

--- WPF Weather/ViewModel/WeatherVM.cs
+++ WPF Weather/ViewModel/WeatherVM.cs
@@ -10,20 +10,28 @@ namespace WPF_Weather.ViewModel
     public class WeatherVM : INotifyPropertyChanged
     {
         private string query;
+
+        // The Query is binded with the Textbox in WeatherWindow, any changes in the textbox will change the Query.
         public string Query
         {
             get { return query; }
             set
             {
                 query = value;
+                // When Query is changed due to the textbox, it will trigger PropertyChanged Event.
                 OnPropertyChanged("Query");
             }
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
 
+        // When Query is changed, this is called. propertyName is Query.
         private void OnPropertyChanged(string propertyName)
         {
+            // Invoke informs the list of subscribers (UI elements bound to Query).
+            // and pass the event information (propertyName which is Query property) to
+            // update the corresponding dependency property on the UI element (when UI loses focus).
+            // "this" refers to the sender of the event which is the instance of the WeatherVM class stored in the UI key.
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
