03b35825c8e3cdf48721def865822d086e2399a8
AndersPeh <pehjyfung@gmail.com>
2025-09-24 04:27:24 +1000

    Data bindings between View and ViewModel

--- WPF Weather/ViewModel/WeatherVM.cs
+++ WPF Weather/ViewModel/WeatherVM.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WPF_Weather.Model;
 
 namespace WPF_Weather.ViewModel
 {
@@ -23,13 +24,50 @@ namespace WPF_Weather.ViewModel
             }
         }
 
+        private CurrentConditions currentConditions;
+
+        public CurrentConditions CurrentConditions
+        {
+            get { return currentConditions; }
+            set
+            {
+                currentConditions = value;
+                OnPropertyChanged("CurrentConditions");
+            }
+        }
+
+        private City selectedCity;
+
+        public City SelectedCity
+        {
+            get { return selectedCity; }
+            set
+            {
+                selectedCity = value;
+                OnPropertyChanged("SelectedCity");
+            }
+        }
+
+        public WeatherVM()
+        {
+            if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
+            {
+                SelectedCity = new City { LocalizedName = "Brisbane" };
+                CurrentConditions = new CurrentConditions
+                {
+                    WeatherText = "Sunny",
+                    Temperature = new Temperature { Metric = new Units { Value = 21 } },
+                };
+            }
+        }
+
         public event PropertyChangedEventHandler? PropertyChanged;
 
-        // When Query is changed, this is called. propertyName is Query.
+        // When Query is changed, this is called. propertyName is what is passed to OnPropertyChanged.
         private void OnPropertyChanged(string propertyName)
         {
-            // Invoke informs the list of subscribers (UI elements bound to Query).
-            // and pass the event information (propertyName which is Query property) to
+            // Invoke informs the list of subscribers (UI elements bound to it in Weather Window).
+            // and pass the event information (propertyName) to
             // update the corresponding dependency property on the UI element (when UI loses focus).
             // "this" refers to the sender of the event which is the instance of the WeatherVM class stored in the UI key.
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
