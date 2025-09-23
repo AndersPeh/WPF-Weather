5e0a8d8437b1a8e3b66809b926696cf4d74bf739
AndersPeh <pehjyfung@gmail.com>
2025-09-24 01:40:02 +1000

    Add project files.

---
+++ WPF Weather/Model/Temperature.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Weather.Model
+{
+    public class Temperature
+    {
+        public Units Metric { get; set; }
+        public Units Imperial { get; set; }
+    }
+}
